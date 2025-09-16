838d7de2bad99084e97a4745ab642632e16d6324
Elias Sylte <eliassy@stud.ntnu.no>
2025-09-16 19:40:20 +0200

    endret tilbake

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-/**
- * Function that greets a person
- * @param firstname First name
- * @param lastname Last name
- */
-
-export const greeting = (firstname: string, lastname: string) => {
-    console.log(`Hello ${firstname} ${lastname}`);
+export const greeting = (name: string) => {
+    console.log(`Hello ${name}`);
 };
